66b06e258d187821247daf738ff89ea9a1fb3c99
AgentD1 <jacobandian3@gmail.com>
2021-10-13 09:53:35 -0400

    Renamed it

--- MusicPic.X/nbproject/Makefile-impl.mk
+++ MusicPic.X/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=UBMP4-Intro-1-Input-Output.X
+PROJECTNAME=MusicPic.X
 
 # Active Configuration
 DEFAULTCONF=free
---
+++ MusicPic.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# free configuration
+CND_ARTIFACT_DIR_free=dist/free/production
+CND_ARTIFACT_NAME_free=MusicPic.X.production.hex
+CND_ARTIFACT_PATH_free=dist/free/production/MusicPic.X.production.hex
+CND_PACKAGE_DIR_free=${CND_DISTDIR}/free/package
+CND_PACKAGE_NAME_free=musicpic.x.tar
+CND_PACKAGE_PATH_free=${CND_DISTDIR}/free/package/musicpic.x.tar
+# pro configuration
+CND_ARTIFACT_DIR_pro=dist/pro/production
+CND_ARTIFACT_NAME_pro=MusicPic.X.production.hex
+CND_ARTIFACT_PATH_pro=dist/pro/production/MusicPic.X.production.hex
+CND_PACKAGE_DIR_pro=${CND_DISTDIR}/pro/package
+CND_PACKAGE_NAME_pro=musicpic.x.tar
+CND_PACKAGE_PATH_pro=${CND_DISTDIR}/pro/package/musicpic.x.tar
